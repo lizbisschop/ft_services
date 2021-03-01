d6fe082d2dffbc56f3c14fb26c2238f4a2d839f4
Liz Bisschop <lbisscho@f1r1s9.codam.nl>
2021-03-01 17:51:58 +0100

    ft_services

--- setup.sh
+++ setup.sh
@@ -1,11 +1,22 @@
 #colors
 red='\033[0;31m'
 
-eval $(minikube docker-env)
+minikube start --vm-driver=virtualbox
 
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+# On first install only
+# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+kubectl apply -f ./srcs/nginx/metalldb_conf.yml
+eval $(minikube docker-env)
 
 echo "BUILDING IMAGES"
 docker build -t nginx ./srcs/nginx
+# docker build -t wordpress ./srcs/wordpress
 
-echo "RUNNING DOCKER"
-docker run -it -p80:80 -p443:443 nginx ./srcs/nginx
+echo "CREATE DEPLOYMENT"
+kubectl create -f ./srcs/nginx/nginx.yml
+# kubectl create -f ./srcs/wordpress/wordpress.yml
+
+kubectl get all
