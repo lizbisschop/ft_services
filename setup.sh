f49eab38aae330c4aca15ba77ed5598f755a25db
Liz Bisschop <lbisscho@f0r4s12.codam.nl>
2021-03-08 18:17:51 +0100

    ft_services

--- setup.sh
+++ setup.sh
@@ -1,25 +1,28 @@
 #colors
-red='\033[0;31m'
+# red='\033[0;31m'
 
-minikube start --vm-driver=virtualbox --addons metalldb --addons dashboard
+minikube start --vm-driver=virtualbox --addons metallb --addons dashboard
+minikube addons enable metallb
+minikube addons enable dashboard
+minikube addons enable metrics-server
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 # On first install only
-# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
-kubectl apply -f ./srcs/nginx/metalldb_conf.yml
-eval $(minikube docker-env)
+kubectl apply -f ./srcs/nginx/metallb_conf.yaml
+# eval $(minikube docker-env)
 
 echo "BUILDING IMAGES"
-docker build -t nginx ./srcs/nginx
-docker build -t mysql ./srcs/mysql
+docker build -t nginx ./s rcs/nginx
+# docker build -t mysql ./srcs/mysql
 docker build -t wordpress ./srcs/wordpress
 
 echo "CREATE DEPLOYMENT"
 kubectl create -k srcs/
 kubectl create -f ./srcs/nginx/nginx.yaml
-# kubectl create -f ./srcs/mysql/mysql.yml
-# kubectl create -f ./srcs/wordpress/wordpress.yml
+# kubectl create -f ./srcs/mysql/mysql.yaml
+# kubectl create -f ./srcs/wordpress/wordpress.yaml
 
 kubectl get all
