850fe4eb8e8f1edb0eb652d429c21c2df3cc5420
Liz Bisschop <lbisscho@f0r6s12.codam.nl>
2021-04-19 17:50:31 +0200

    trying still

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ kubectl apply -f ./srcs/secret.yaml
 
 echo "BUILDING IMAGES"
 docker build -t nginx ./srcs/nginx
-docker build -t mysql ./srcs/mysql2
+docker build -t mysql ./srcs/mysql
 docker build -t phpmyadmin ./srcs/phpmyadmin
 docker build -t wordpress ./srcs/wordpress
 docker build -t influxdb ./srcs/influxdb
@@ -32,7 +32,7 @@ docker build -t grafana ./srcs/grafana
 echo "CREATE DEPLOYMENT"
 
 kubectl apply -f ./srcs/nginx/nginx.yaml
-kubectl apply -f ./srcs/mysql2/mysql.yaml
+kubectl apply -f ./srcs/mysql/mysql.yaml
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
 kubectl apply -f ./srcs/wordpress/wordpress.yaml
 kubectl apply -f ./srcs/influxdb/influxdb.yaml
